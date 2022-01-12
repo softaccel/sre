77b7002731a186c3c7e36c55fcbbd0bb85bbbc73
Sergiu Voicu <sergiu.voicu@softaccel.net>
2022-01-12 18:01:25 +0200

    some update

--- static/assets/js/apiator.js
+++ static/assets/js/apiator.js
@@ -1,7 +1,109 @@
 
 
-
 (function ($) {
+	var escapes = {
+		"'": "'",
+		'\\': '\\',
+		'\r': 'r',
+		'\n': 'n',
+		'\u2028': 'u2028',
+		'\u2029': 'u2029'
+	};
+
+	var escapeRegExp = /\\|'|\r|\n|\u2028|\u2029/g;
+
+	function escapeChar(match) {
+		return '\\' + escapes[match];
+	}
+
+// JavaScript micro-templating, similar to John Resig's implementation.
+// Underscore templating handles arbitrary delimiters, preserves whitespace,
+// and correctly escapes quotes within interpolated code.
+// NB: `oldSettings` only exists for backwards compatibility.
+	function template(text, settings, oldSettings) {
+		if (!settings && oldSettings) settings = oldSettings;
+		let tplSettings = {
+			evaluate: /<%([\s\S]+?)%>/g,
+			interpolate: /<%=([\s\S]+?)%>/g,
+			escape: /<%-([\s\S]+?)%>/g
+		};
+		if(!settings) {
+			settings = {};
+		}
+		Object.assign(settings,tplSettings);
+		if(!settings.evaluate) {
+			settings.evaluate = tplSettings;
+		}
+		if(!settings.interpolate) {
+			settings.interpolate = tplSettings;
+		}
+		if(!settings.escape) {
+			settings.escape = tplSettings;
+		}
+
+		// settings = defaults({}, settings, _$1.templateSettings);
+
+		// Combine delimiters into one regular expression via alternation.
+		var matcher = RegExp([
+			(settings.escape || noMatch).source,
+			(settings.interpolate || noMatch).source,
+			(settings.evaluate || noMatch).source
+		].join('|') + '|$', 'g');
+
+		// Compile the template source, escaping string literals appropriately.
+		var index = 0;
+		var source = "__p+='";
+		text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+			source += text.slice(index, offset).replace(escapeRegExp, escapeChar);
+			index = offset + match.length;
+
+			if (escape) {
+				source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
+			} else if (interpolate) {
+				source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+			} else if (evaluate) {
+				source += "';\n" + evaluate + "\n__p+='";
+			}
+
+			// Adobe VMs need the match returned to produce the correct offset.
+			return match;
+		});
+		source += "';\n";
+
+		var argument = settings.variable;
+		if (argument) {
+			// Insure against third-party code injection. (CVE-2021-23358)
+			if (!bareIdentifier.test(argument)) throw new Error(
+				'variable is not a bare identifier: ' + argument
+			);
+		} else {
+			// If a variable is not specified, place data values in local scope.
+			source = 'with(obj||{}){\n' + source + '}\n';
+			argument = 'obj';
+		}
+
+		source = "var __t,__p='',__j=Array.prototype.join," +
+			"print=function(){__p+=__j.call(arguments,'');};\n" +
+			source + 'return __p;\n';
+
+		var render;
+		try {
+			render = new Function(argument, '_', source);
+		} catch (e) {
+			e.source = source;
+			throw e;
+		}
+
+		var template = function(data) {
+			return render.call(this, data, _$1);
+		};
+
+		// Provide the compiled source as a convenience for precompilation.
+		template.source = 'function(' + argument + '){\n' + source + '}';
+
+		return template;
+	}
+
 	FrontBone = {};
 	let itemsArr = {};
 	let db = {};
@@ -894,6 +996,7 @@
 				.replace(/&amp;/gi, "&");
 
 			params = {
+				// template: template(html),
 				template: _.template(html),
 				el: $el
 			};
@@ -1768,6 +1871,7 @@
 			.replace(/&nbsp;/gi, " ")
 			.replace(/&amp;/gi, "&");
 		options.template = _.template(templateTxt);
+		// options.template = template(templateTxt);
 
 
 		let collectionConfig = {
@@ -1921,6 +2025,7 @@
 			.replace(/&nbsp;/gi, " ")
 			.replace(/&amp;/gi, "&");
 		options.template = _.template(templateTxt);
+		// options.template = template(templateTxt);
 
 		return Item(options).bindView(ItemView({
 			template: options.template,
